db93e095e6c6e68509ff902000a7a69821a2a91c
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2024-09-17 20:47:37 +0200

    Création branch develop

---
+++ script.js
@@ -0,0 +1,7 @@
+let motTapeOk = false 
+
+if(motTapeOk) {
+    console.log("Bravo, vous avez correctement tapé le mot")
+} else {
+    console.log("Echec, vous n'avez pas correctement tapé le mot")
+}
