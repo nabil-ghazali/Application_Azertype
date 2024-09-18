91f1f09d11d842d0157042166bee3e3ea6a2ee4a
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2024-09-18 15:05:50 +0200

    test affichage mot, saisi et score

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-let motTapeOk = false 
+let listeMots = ["Cachalot", "Pétunia", "Serviette"]
+let score = 0
+let motUtilisateur = []
+    motUtilisateur = prompt("veuillez entrez les mots suivants :" + listeMots[0])
+    if (motUtilisateur === listeMots[0]){
+        score += 1;
+    }
 
-if(motTapeOk) {
-    console.log("Bravo, vous avez correctement tapé le mot")
-} else {
-    console.log("Echec, vous n'avez pas correctement tapé le mot")
-}
+    motUtilisateur = prompt("veuillez entrez les mots suivants :" + listeMots[1])
+    if (motUtilisateur === listeMots[1]){
+        score += 1;
+    }
+
+    motUtilisateur= prompt("veuillez entrez les mots suivants :" + listeMots[2])
+    if (motUtilisateur === listeMots[2]){
+        score += 1;
+    }
+    console.log("Score :", score)
