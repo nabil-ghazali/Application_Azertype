8877d797010bb318ff586203663b059421f46716
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2024-09-19 23:26:52 +0200

    Refactorisation de code de saisi utilisateur

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
-let listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
 let score = 0
 let motUtilisateur = []
 
 // Vérification du mot saisi par l'utilisateur : on vérifie qu'il correspond au premier mot du tableau "listeMots"
-    motUtilisateur = prompt("veuillez entrez les mots suivants :" + listeMots[0])
+for (let i = 0; i < 3; i = i + 1 ) {
+    motUtilisateur = prompt("veuillez entrez les mots suivants :" + listeMots[i])
+
     if (motUtilisateur === listeMots[0]){
         score += 1;
-    }
-
-// Vérification du mot saisi par l'utilisateur : on vérifie qu'il correspond au premier mot du tableau "listeMots"
-    motUtilisateur = prompt("veuillez entrez les mots suivants :" + listeMots[1])
-    if (motUtilisateur === listeMots[1]){
+    }else if (motUtilisateur === listeMots[1]){
         score += 1;
-    }
-
-// Vérification du mot saisi par l'utilisateur : on vérifie qu'il correspond au premier mot du tableau "listeMots"
-    motUtilisateur= prompt("veuillez entrez les mots suivants :" + listeMots[2])
-    if (motUtilisateur === listeMots[2]){
+    }else if (motUtilisateur === listeMots[2]){
         score += 1;
     }
 
 // Affichage du score de l'utilisateur    
-    console.log("Score :", score)
+    
+}
+
+console.log("Score :", score)
