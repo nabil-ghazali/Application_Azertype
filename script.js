fdf74b02017aa3d681438b9b49fbec69cd01d946
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2024-09-19 23:43:45 +0200

    Refactorisation de code de saisi utilisateur avec boucle

--- script.js
+++ script.js
@@ -3,14 +3,9 @@ let score = 0
 let motUtilisateur = []
 
 // Vérification du mot saisi par l'utilisateur : on vérifie qu'il correspond au premier mot du tableau "listeMots"
-for (let i = 0; i < 3; i = i + 1 ) {
+for (let i = 0; i < listeMots.length; i++  ) {
     motUtilisateur = prompt("veuillez entrez les mots suivants :" + listeMots[i])
-
-    if (motUtilisateur === listeMots[0]){
-        score += 1;
-    }else if (motUtilisateur === listeMots[1]){
-        score += 1;
-    }else if (motUtilisateur === listeMots[2]){
+    if (motUtilisateur === listeMots[i]){
         score += 1;
     }
 
