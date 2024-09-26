ec84582a6027f9d9d41181bb9302160fe5fb9b12
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2024-09-26 23:48:31 +0200

    to transform into a function

--- script.js
+++ script.js
@@ -1,38 +1,49 @@
-const listeMots = ["Cachalot", "Pétunia", "Serviette"]
-
-let score = 0
-let motUtilisateur = []
-let choixUtilisateur = ""
-
-//function lancerBoucleDeJeu
-
-//Choix de l'utilisateur entre phrases ou mots
-while(motUtilisateur !== 'mots' || motUtilisateur !=='phrases'){
-motUtilisateur = prompt("Veuillez choisir entre 'mots' ou 'phrases' : ")
-
-    if(motUtilisateur === 'mots'){
-        console.log('mots')
+// Affichage du résultat de l'utilisateur   
+function afficherResultat(score, nbMotsProposes){
+        console.log("Votre score est de : "+ score + 'sur' + nbMotsProposes)
         
-    }else if(motUtilisateur === 'phrases'){
-        console.log('phrases')
     }
+
+//function choix de l'utilisateur entre phrases ou mots
+function choisirPhrasesOuMots (){
+    let choix = prompt ('Voulez vous jouez avec les mots (entrez "mots") ou les phrases (entrez "phrases")?')
+        while(choix !== 'mots' && choix !=='phrases'){
+            choix = prompt("Veuillez choisir entre 'mots' et 'phrases' : ")
+        }
+        return choix
+    }
+     
+    
+function lancerBoucleDeJeu(listeProposition){
+// On lui demande de taper les mots ou phrases de la liste
+    let score = 0
+    for (let i = 0; i < listeProposition.length; i++  ) {
+        motUtilisateur = prompt("veuillez entrez les mots suivants :" + listeProposition[i])
+        // Vérification du mot saisi par l'utilisateur avec les mots de notre liste : 
+        if (motUtilisateur === listeProposition[i]){
+                score ++;
+        }
+    }
+    return score
 }
 
-console.log(motUtilisateur)
 
-// Vérification du mot saisi par l'utilisateur : on vérifie qu'il correspond au premier mot du tableau "listeMots"
-// for (let i = 0; i < listeMots.length; i++  ) {
-//     motUtilisateur = prompt("veuillez entrez les mots suivants :" + listeMots[i])
-//     if (motUtilisateur === listeMots[i]){
-//         score += 1;
-//     }
+function lancerJeu(){
+    let choix = choisirPhrasesOuMots ()
+    let score = 0
+    let nbMotsProposes = 0
 
-// }
+//Si l'utilisateur choisi "mots", on lui demande de taper les mots suivants : 
+    if(choix === "mots"){
+        score = lancerBoucleDeJeu(listeMots)
+        nbMotsProposes = listeMots.length
+//Si l'utilisateur choisi "mots", on lui demande de taper les mots suivants : 
+    } else {
+        score = lancerBoucleDeJeu(listePhrases)
+        nbMotsProposes = listePhrases.length
+    }
 
-// function retournerMessageScore(score, nombreQuestions){
-//     let message = "Votre score est de :"+ score + "sur " + nombreQuestions
-//     return message
-// }
+    afficherResultat(score, nbMotsProposes)
+}
 
-// Affichage du score de l'utilisateur    
-// prompt(retournerMessageScore(score, listeMots.length))
+lancerJeu()
