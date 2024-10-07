5985ce51329fbb5b18ee43ca6e05e196fde01543
nabil_ghazali <nabil.dev.ghaz@hotmail.com>
2024-10-07 21:27:18 +0200

    share the score and send email

--- scripts/script.js
+++ scripts/script.js
@@ -19,18 +19,6 @@ function afficherResultat(score, nbMotsProposes){
         
     }
 
-
-/**
- * Cette fonction construit et affiche l'email. 
- * @param {string} nom : le nom du joueur
- * @param {string} email : l'email de la personne avec qui il veut partager son score
- * @param {string} score : le score. 
- */
-function afficherEmail(nom, email, score) {
-    let mailto = `mailto:${email}+?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je vais de réaliser le score ${score} sur le site d'Azertype !`
-    location.href = mailto
-}
-
 /**
  * Cette fonction affiche une proposition, que le joueur devra recopier, 
  * dans la zone "zoneProposition"
@@ -39,6 +27,17 @@ function afficherEmail(nom, email, score) {
 function afficherProposition(motAfficher){
     let zoneProposition = document.querySelector(".zoneProposition"); 
     zoneProposition.innerHTML = motAfficher
+}
+
+/**
+ * Cette fonction construit et affiche l'email. 
+ * @param {string} nom : le nom du joueur
+ * @param {string} email : l'email de la personne avec qui il veut partager son score
+ * @param {string} score : le score. 
+ */
+function afficherEmail(nom, email, score) {
+    let mailto = `mailto:${email}+?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site d'Azertype !`
+    location.href = mailto
 }
 
 /**
@@ -79,7 +78,7 @@ function lancerJeu(){
 // Boutons radio : choix entre "mots" et "phrases"
 let listBtnRadio = document.querySelectorAll('.optionSource input')
 
-    for( let index = 0; i < listBtnRadio.length; index++) {
+    for( let index = 0; index < listBtnRadio.length; index++) {
         // lors de l'appui du bouton radio on recupère l'evenement du changement 
         listBtnRadio[index].addEventListener("change", (event)=>{            
             console.log(event.target.value)
@@ -87,23 +86,28 @@ let listBtnRadio = document.querySelectorAll('.optionSource input')
             if(event.target.value === "1"){
                 listPropositions = listeMots
             }else if(event.target.value === "2"){
-                listPropositions =listePhrases
+                listPropositions = listePhrases
             }
             //on affiche la proposition mise à jour selon le choix au click du bouton radio
             afficherProposition(listPropositions[i])
 
-    })
+        })
     }
 
+    
+    let form = document.querySelector('form')
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let nom = document.getElementById("nom").value;
+        console.log(nom)
+    
+        let email = document.getElementById("email").value;
+        console.log(email)
+
+        let scoreEmail = `${score} / ${i}`
+
+        afficherEmail(nom, email, scoreEmail)
+    });
+    
      afficherResultat(score, i)
 }
-
-let form = document.querySelector("form")
-
-form.addEventListener("submit", (event) => {
-   event.preventDefault()
-   let nom = document.getElementById("nom")
-   console.log(nom.value)
-   let email = document.getElementById("email")
-   console.log(email.value)
-})
